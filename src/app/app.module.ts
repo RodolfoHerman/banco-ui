3f300a39e9bfb3113d1c0c775b1056faa265532f
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2019-09-05 18:53:42 -0300

    Criação dos componentes Home, Sobre e Cabecalho. Configuração inicial das Rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    CabecalhoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cabecalho/cabecalho.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cb-cabecalho',
+  templateUrl: './cabecalho.component.html'
+})
+export class CabecalhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
